ee726ef88139c92fbc621fda7b00901db614a321
Jess Telford <jtelford@groupon.com>
2015-01-12 21:24:47 +1100

    Previous hashes in forkdb for an idea

--- actions/post_idea/index.js
+++ actions/post_idea/index.js
@@ -13,9 +13,27 @@ function escapeHash(hash) {
   return hash;
 }
 
+/**
+ * @param id String The id to escape
+ * @return String The escaped id. `undefined` if `id` not set.
+ */
 function escapeId(id) {
   // TODO: Implement me
   return id;
+}
+
+/**
+ * @param key String The ForkDB key to add
+ * @param prevData Array The array of previous hashes to process
+ * @return Array [ { key: '<the key>', hash: '<the hash>' } ]
+ */
+function addKeyToPrev(key, prevData) {
+  return prevData.map(function(prev) {
+    return {
+      key: key,
+      hash: prev
+    }
+  });
 }
 
 module.exports = function() {
@@ -31,16 +49,11 @@ module.exports = function() {
       return next(validationError);
     }
 
-    // The idea's ID
-    if (ideaData.id) {
-      writeMeta.key = escapeId(ideaData.id);
-    } else {
-      writeMeta.key = cuid();
-    }
+    // The idea's ID - generate a new one if one doesn't exist
+    writeMeta.key = escapeId(ideaData.id) || cuid();
 
-    if (ideaData.prev) {
-      writeMeta.prev = escapeId(ideaData.prev);
-    }
+    // Generate the appropriate 'prev' data
+    writeMeta.prev = addKeyToPrev(writeMeta.key, ideaData.prev || []);
 
     // Write the Idea data
     ideaDb

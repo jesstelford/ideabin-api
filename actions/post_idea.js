29d3c90e9406c8bab22e935dbbba29a72c26db9e
Jess Telford <jtelford@groupon.com>
2015-01-12 14:53:32 +1100

    POSTing an idea JSON and storing it in forkdb

--- actions/post_idea.js
+++ actions/post_idea.js
@@ -1,10 +1,45 @@
-var dispatch = require('../dispatch'),
-    dbManager = require('../dbManager');
+var cuid = require('cuid'),
+    dispatch = require('../dispatch'),
+    dbManager = require('../dbManager'),
+    actionUtils = require('./utils');
+
+function escapeHash(hash) {
+  // TODO: Implement me
+  return hash;
+}
+
+function escapeId(id) {
+  // TODO: Implement me
+  return id;
+}
 
 module.exports = function() {
 
+  var ideaDb = dbManager.get('ideas');
+
   dispatch.on('action:postIdea', function(ideaData, req, res, next) {
-    res.json(ideaData);
+
+    var requiredError = actionUtils.checkRequiredParams(['idea'], ideaData);
+
+    if (requiredError) {
+      return next(requiredError);
+    }
+
+    if (ideaData.id) {
+      ideaData.id = escapeId(ideaData.id);
+    } else {
+      ideaData.id = cuid();
+    }
+
+    ideaDb
+      .createWriteStream({key: ideaData.id}, function(err, key) {
+        if (err) {
+          return next(restify.restError('blah'));
+        }
+
+        res.json(ideaData);
+      })
+      .end(ideaData.idea);
   });
 
 }

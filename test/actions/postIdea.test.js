82cdc6463df506b2cfde2cb97f9dd13ecfd5ca2d
Jess Telford <jtelford@groupon.com>
2015-01-20 08:38:35 +1100

    Factor out soon-to-be-common part of test

--- test/actions/postIdea.test.js
+++ test/actions/postIdea.test.js
@@ -4,6 +4,21 @@ var sinon = require('sinon'),
     assert = require('assert'),
     postIdea = require('../../actions/post_idea/handler');
 
+function testPost(ideaData, callback) {
+
+  var req = {},
+      res = {
+        send: sinon.spy(),
+        json: sinon.spy()
+      };
+
+  postIdea(ideaData, req, res, function() {
+    var args = [req, res].concat(arguments);
+    callback.apply(this, args);
+  });
+
+}
+
 describe('POST idea', function() {
 
   it('saves to database', function(done) {
@@ -12,14 +27,9 @@ describe('POST idea', function() {
           name: 'foo'
         },
         owner: 'jess'
-      },
-      req = {},
-      res = {
-        send: sinon.spy(),
-        json: sinon.spy()
       };
 
-    postIdea(ideaData, req, res, function() {
+    testPost(ideaData, function(req, res) {
 
       assert.equal(res.send.callCount, 0);
       assert.equal(typeof res.json.args[0][0], 'object');

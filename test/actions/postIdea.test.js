68b776c2bddf96252d77468f9c21665d7d1ecd10
Jess Telford <jtelford@groupon.com>
2015-01-22 08:24:58 +1100

    Test data is written to correct database

--- test/actions/postIdea.test.js
+++ test/actions/postIdea.test.js
@@ -2,7 +2,9 @@
 
 var sinon = require('sinon'),
     assert = require('assert'),
-    postIdea = require('../../actions/post_idea/handler');
+    postIdea = require('../../actions/post_idea/handler'),
+    dbManager = require('../../dbManager'),
+    concatStreamCallback = require('concat-stream-callback');
 
 function testPost(ideaData, callback) {
 
@@ -54,6 +56,48 @@ describe('POST idea', function() {
       assert.equal(res.send.args[0][0], 409);
       assert.equal(res.send.args[0][1].message, '`idea` is required');
       done();
+
+    });
+
+  });
+
+  it('writes correct parts to correct DBs', function(done) {
+
+    var ideaDb = dbManager.get('ideas'),
+        // See actions/post_idea/schema.orderly
+        ideaData = {
+          idea: {
+            name: 'bar'
+          },
+          owner: 'jess'
+        };
+
+    testPost(ideaData, function(req, res) {
+
+      var hash = res.json.args[0][0].hash;
+      var key = res.json.args[0][0].id;
+
+      // The main idea data should exist as the forkdb blob
+      concatStreamCallback(
+        ideaDb.createReadStream(hash),
+        {encoding: 'string'},
+        function (readErr, blob) {
+
+          if (readErr) { throw readErr; }
+          assert.deepEqual(JSON.parse(blob), ideaData.idea);
+
+          // The meta data is stored in the meta section of forkdb
+          ideaDb.get(hash, function(metaErr, meta) {
+            if (metaErr) { throw metaErr; }
+            assert.deepEqual(meta, {
+              key: key,
+              owner: ideaData.owner,
+              prev: []
+            });
+            done();
+          });
+        }
+      );
 
     });
 
